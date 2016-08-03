b6e3dbc84f806b40c4f0c4b3aeee24ac9deb1571
Mike Peiman <mikepeiman@gmail.com>
2016-08-03 14:09:21 -0400

    Added Twitter Bootstrap via meteor add twbs:bootstrap@3.3.6. also added main.css

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -5,8 +5,16 @@ const ImageDetail = (props) => {
 	// props.image.title
 	// props.image.link
 	return (
-		<div>{props.image.title}</div>
-	
+		<li className="media list-group-item">
+		<div className="media-left">
+			<img src={props.image.link} />
+		</div>
+		<div className="media-body">
+			<h4 className="media-heading">
+			{props.image.title}
+			</h4>
+		</div>
+		</li>
 	);
 };
 
--- client/components/image_list.js
+++ client/components/image_list.js
@@ -5,13 +5,13 @@ import ImageDetail from './image_detail';
 
 // create a dummy list of images so we have boilerplate data
 const IMAGES = [
-	{ title: 'Pen', link: 'http://dummyimage.com/600x400/000/fff' },
-	{ title: 'Pine Tree', link: 'http://dummyimage.com/600x400/000/fff' },
-	{ title: 'Mug', link: 'http://dummyimage.com/600x400/000/fff' }
+	{ title: 'Pen', link: 'http://dummyimage.com/300x200/000/fff' },
+	{ title: 'Pine Tree', link: 'http://dummyimage.com/300x200/000/fff' },
+	{ title: 'Mug', link: 'http://dummyimage.com/300x200/000/fff' }
 ];
 
-/* import is not just used for NPM modules; 
- we can also import components from other files */
+// import is not just used for NPM modules; 
+//we can also import components from other files 
 
 // create our component
 // props = propeties; anything we pass from a parent to a child
@@ -20,7 +20,7 @@ const ImageList = () => {
 		return <ImageDetail image={image} />
 	});
 	return (
-		<ul>
+		<ul className="media-list list-group">
 			{RenderedImages}
 		</ul>
 	);
